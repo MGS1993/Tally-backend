161e5ba05be0cb60314d4962894b8c5f383e76fa
Manuel <m_guzman74@yahoo.com>
2021-11-28 04:05:55 -0600

    ts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const apiRoutes = require("./routes/api");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,11 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
 
 const port = process.env.PORT || 9000;
 
