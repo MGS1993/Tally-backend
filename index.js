6a0ef8e42bf62efa49a9286baa7dcedac9792663
Manuel <m_guzman74@yahoo.com>
2021-11-28 04:16:59 -0600

    ts

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "./build")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build"));
+    res.sendFile(path.join(__dirname, "./build"));
   });
 }
 
