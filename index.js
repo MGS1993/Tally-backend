06c80c55f774e658d5c3a1dd022206237fe814a0
Manuel <m_guzman74@yahoo.com>
2021-11-28 22:02:15 -0600

    preparing for heroku deployment

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const apiRoutes = require("./routes/api");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
+app.use(express.static("public"));
 app.use(express.json());
 app.use("/api", apiRoutes);
 
@@ -12,6 +14,10 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 const port = process.env.PORT || 9000;
 
